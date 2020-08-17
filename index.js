4c8f0b7d5abd32c02b1d35d34eb87faed1052e13
furkan116 <51910163+furkan116@users.noreply.github.com>
2020-08-17 10:34:10 +0300

    v0.8

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var allButtons = document.querySelectorAll('.featured-button');
-console.log("Found", allButtons.length, "div which class starts with “button”.");
+let allControl = "all";
 
 allButtons.forEach(function(e) {
     e.addEventListener('click', function() {
@@ -7,23 +7,20 @@ allButtons.forEach(function(e) {
         toggle(filter)
     });  
 })
-var test = 0;
 
 function toggle(key) {
     const items = document.querySelectorAll('#featured-product-control')
-    var test = 0;
-
+    
     items.forEach(function(e) {
-        if (key == e.className) {
+        if(key == allControl) {
+            e.setAttribute("style", "display : block");
+        }
+        else if (key == e.className) {
             e.setAttribute("style", "display : block");
         }
         else {
             e.setAttribute("style", "display : none");
         }
-        test = test + 1;
-        console.log(e.className);
-        console.log(key);
     })
-    console.log(items, key);
-    console.log(test);
+
 }
